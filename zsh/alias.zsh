1c6792e130b0f3cab1aa5dbd2fdd8f56e0761a10
Kevin Cai <kevin@kevincai.com>
2019-07-20 23:43:34 -0700

    Added some git aliases

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -11,7 +11,6 @@ alias gbr='git branch'
 alias gbr!="git branch | grep \* | cut -d ' ' -f2 | pbcopy"
 alias gco='git checkout'
 alias gcm='git commit'
-alias gdf='git diff'
 alias glg='git log'
 alias gpl='git pull'
 alias gps='git push'
@@ -20,3 +19,29 @@ alias grs='git reset'
 alias grs!='git reset HEAD^ --soft && git reset' # Reset to working directory for prev commit
 alias gst='git status'
 alias gsh='git stash'
+alias gwip='git commit -m "WIP" -n'
+
+alias trash='trash -F'
+
+# Alias "with" arguments
+function gdf() {
+  if [[ $1 == "s" ]]; then
+    git diff --staged
+  else
+    git diff $@
+  fi
+}
+
+# Some repos have shared precommit hooks, making a git shim here allows working around them
+function git () {
+  if [[ $1 == "commit" ]]; then
+    is-branch-wip
+    if [[ $? != 0 ]]; then
+      return 1
+    fi
+  fi
+  command git $@
+  if [[ $1 == "checkout" ]]; then
+    is-branch-wip
+  fi
+}
